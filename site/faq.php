8fba50f2572ec2ecc56eb63112b668dc2b47f20d
mike_55 <josemiguelsribeiro@hotmail.com>
2014-01-04 16:34:01 +0000

    faq e sobre

---
+++ site/faq.php
@@ -0,0 +1,35 @@
+﻿<?php
+	include "functions.php";
+	
+	sec_session_start();
+				
+	if(login_check() == true)
+	{			// PROTECTED PAGE HERE
+	
+		ligar_BD();
+		$query = "SELECT * FROM utilizador 	
+		WHERE id = \"".$_SESSION['user_id']."\"";
+		$result = mysql_query($query);				//só para poder mostrar os dados pessoais na barra lateral
+?>
+		<html>
+			<link rel="stylesheet" type="text/css" href="estilos.css" media="screen" />
+			<body>
+				<div class="body">
+					<?php cabecalho(2); ?>
+					<div class="main">
+						<p>FAW</p>
+						<table style="border:1px solid white">
+							<tr><td>P: O que é isto ?</td></tr>
+							<tr><td>R: Isto é um site de carpool ....</td></tr>
+						</table>
+					</div>
+					<?php dadosPessoais(0, $result); ?>
+					<?php rodape(); ?>
+				</div>
+			</body>
+		</html>
+<?php
+	} 
+	else
+		header("LOCATION: erro_sessao.php");
+?>
---
+++ site/sobre.php
@@ -0,0 +1,32 @@
+﻿<?php
+	include "functions.php";
+	
+	sec_session_start();
+				
+	if(login_check() == true)
+	{			// PROTECTED PAGE HERE
+	
+		ligar_BD();
+		$query = "SELECT * FROM utilizador 	
+		WHERE id = \"".$_SESSION['user_id']."\"";
+		$result = mysql_query($query);				//só para poder mostrar os dados pessoais na barra lateral
+?>
+		<html>
+			<link rel="stylesheet" type="text/css" href="estilos.css" media="screen" />
+			<body>
+				<div class="body">
+					<?php cabecalho(2); ?>
+					<div class="main">
+						<p>Sobre</p>
+						<p>Isto é um site de carpool ....</p>
+					</div>
+					<?php dadosPessoais(0, $result); ?>
+					<?php rodape(); ?>
+				</div>
+			</body>
+		</html>
+<?php
+	} 
+	else
+		header("LOCATION: erro_sessao.php");
+?>
